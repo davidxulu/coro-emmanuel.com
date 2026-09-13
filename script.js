cd5531c53757cf5b943eb23ca59d63731084210e
davidxulu <oscarxulu7@gmail.com>
2026-09-13 13:27:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -58,11 +58,11 @@ let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => 
 
 // Tiempos litúrgicos: colores oficiales + IDs de YouTube
 const liturgicalTimes = [
-  { id: 'adviento',  label: 'Adviento',         color: '#6B3FA0', youtubeId: 'PON_ID_ADVIENTO',  poster: 'img/videos/adviento.webp' },
+  //{ id: 'adviento',  label: 'Adviento',         color: '#6B3FA0', youtubeId: 'ID_ADVIENTO',  poster: 'img/videos/adviento.webp' },
   { id: 'navidad',   label: 'Navidad',          color: '#D4AF37', youtubeId: 'IYwSWN_G4i4',   poster: 'img/videos/navidad.webp' },
   { id: 'cuaresma',  label: 'Cuaresma',         color: '#5C3B70', youtubeId: 'fOnHdXtAojQ',  poster: 'img/videos/cuaresma.webp' },
   { id: 'pascua',    label: 'Pascua',           color: '#F2C14E', youtubeId: 'Xpfun_Uh5Wg?feature=share',    poster: 'img/videos/pascua.webp' },
-  { id: 'ordinario', label: 'Tiempo Ordinario', color: '#4C8C4A', youtubeId: 'PON_ID_ORDINARIO', poster: 'img/videos/ordinario.webp' },
+  { id: 'ordinario', label: 'Tiempo Ordinario', color: '#4C8C4A', youtubeId: 'l6yHgmo0fEc', poster: 'img/videos/ordinario.webp' },
 ];
 
 App.prototype.selectLiturgicalTime = function(event) {
